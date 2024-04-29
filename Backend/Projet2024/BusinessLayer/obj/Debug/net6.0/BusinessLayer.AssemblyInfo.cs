939efdabd39593c05c12ff0f1f59fb25cbf48432
Boubsila Radouane <PSR12216@students.ephec.be>
2024-04-30 01:35:08 +0200

    create file back en front structure and some fonction of courses

--- Backend/Projet2024/BusinessLayer/obj/Debug/net6.0/BusinessLayer.AssemblyInfo.cs
+++ Backend/Projet2024/BusinessLayer/obj/Debug/net6.0/BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7063423905e24acfb9e157bc3e0cdf538f33031c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Backend/Projet2024/Presentation/Program.cs
+++ Backend/Projet2024/Presentation/Program.cs
@@ -1,24 +1,31 @@
 using BusinessLayer;
 using DataAccesLayer;
+using Microsoft.Extensions.Primitives;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Add services of interfaces
+builder.Services.AddScoped<ICourseService, CourseServices>();
+builder.Services.AddScoped<ICourseRepository, CourseRepository>();
 
-// add service of interface ICourseService
+// Resolve CORS issue whith alow frontend with port 4200 to have acces to the backend
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
-builder.Services.AddScoped <ICourseService,CourseServices>();
-
-// add service of interface ICourseRepository
-
-builder.Services.AddScoped <ICourseRepository,CourseRepository>();
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:4200")
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
@@ -30,9 +37,31 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseRouting();
+
+// Apply CORS policy
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+// create custom middleware 
+app.Use(async (context, next) =>
+{
+    context.Request.Headers.TryGetValue("user-agent", out StringValues headerValue);
+    if(headerValue.ToString().Contains("Edg"))
+        await next();
+    else
+    {
+        context.Response.StatusCode = 412;
+        await context.Response.WriteAsync("Please use chrome " + " " + headerValue);
+    }
+}
+
+);
+
+
+
 
 app.Run();
